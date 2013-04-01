6ac9c0a47c604ef2df1c8a535b7fb9db6fe3369d
Román Gorojovsky Sánchez <rgoro@GreatAtuin.(none)>
2013-04-01 13:35:10 -0300

    New Install sabe bajar todo esto de github

--- new_install.sh
+++ new_install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
-apt-get dist-upgrade -y
+sudo apt-get dist-upgrade -y
 
-apt-get autoremove -y
+sudo apt-get autoremove -y
 
-apt-get install -y vim vim-gtk chromium-browser calibre yakuake skype git youtube-dl ipython htop pidgin-plugin-pack filezilla chromium-codecs-ffmpeg-extra texlive-latex-base texlive-latex-recommended vim-latexsuite exuberant-ctags
+sudo apt-get install -y vim vim-gtk chromium-browser calibre yakuake skype git youtube-dl ipython htop pidgin-plugin-pack filezilla chromium-codecs-ffmpeg-extra texlive-latex-base texlive-latex-recommended vim-latexsuite exuberant-ctags
 
+if [ ! -d ~/bin ]; then
+	cd ~
+	git clone https://github.com/rgoro/home-bin.git bin
+fi
+
+cd ~/bin
+
+cp conf/.* ~  
 
