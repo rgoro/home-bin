577cdf47022bc848fcd8e49c1db6c5a8c25247ea
Román Gorojovsky Sánchez <rgoro@roman-INRIA.(none)>
2013-04-24 15:53:16 +0200

    paquetes para INRIA

--- new_install.sh
+++ new_install.sh
@@ -8,10 +8,15 @@ sudo apt-get autoremove -y
 
 sudo apt-get install -y vim vim-gtk chromium-browser calibre yakuake skype git youtube-dl ipython htop pidgin-plugin-pack filezilla chromium-codecs-ffmpeg-extra texlive-latex-base texlive-latex-recommended vim-latexsuite exuberant-ctags lame easytag
 
+#INRIA
+sudo apt-get install -i g++ libjsoncpp-dev
+
 if [ ! -d ~/bin ]; then
 	cd ~
 	git clone https://github.com/rgoro/home-bin.git bin
 fi
+
+#Debería agregar los comandos de git para agregar los aliases
 
 cd ~
 
