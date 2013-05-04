4dfb3294f4fbc2a95021a6e4209196c7264c0f01
Román Gorojovsky Sánchez <rgoro@GreatAtuin.(none)>
2013-05-04 11:29:44 +0200

    usemos el parámetro caramba

--- resize_fotos.sh
+++ resize_fotos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -e "$i" ]; then
-	cat fotos.txt | xargs -I FOTO convert FOTO -verbose -resize 40% menor_FOTO
+	cat "$i" | xargs -I FOTO convert FOTO -verbose -resize 40% menor_FOTO
 else
 	echo "$1 no existe"
 fi
