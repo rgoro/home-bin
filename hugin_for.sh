0732f48c089ebc66b6caf1b640b594d16b7eb675
Román <rgoro@GreatAtuin.(none)>
2013-12-05 16:17:28 -0300

    para lanzar panos más rápido

---
+++ hugin_for.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo "USO: $0 inicio fin"
+	exit 1
+fi
+
+START=$1
+END=$2
+
+FILES=""
+
+for i in $(seq $START $END); do
+	FILES="$FILES IMG_$i.JPG"
+done
+
+hugin $FILES &
+ &
