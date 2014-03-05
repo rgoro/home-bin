da811ef7a4eed26b32f3e1cc509c4e0ff7c4b865
Román Gorojovsky <roman@monarlab.com>
2014-03-05 14:12:15 -0300

    Solo lo estrictamente necesario

--- new_install.sh
+++ new_install.sh
@@ -7,13 +7,16 @@ sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 
 #Basicos
-sudo apt-get install -y sl vim vim-gtk chromium-browser calibre yakuake skype git ipython htop pidgin-plugin-pack filezilla chromium-codecs-ffmpeg-extra texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-lang-spanish vim-latexsuite exuberant-ctags subversion tidy vim-addon-manager
+sudo apt-get install -y sl vim vim-gtk chromium-browser calibre git ipython htop pidgin-plugin-pack filezilla chromium-codecs-ffmpeg-extra  exuberant-ctags subversion tidy vim-addon-manager
+
+#Basicos II
+#sudo apt-get install yakuake texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-lang-spanish vim-latexsuite 
 
 # Solo para casa
 # sudo apt-get install youtube-dl lame easytag hugin mplayer
 
 #Activar el addon
-vim-addons install latex-suite
+#vim-addons install latex-suite
 
 #INRIA
 #sudo apt-get install -y g++ libjsoncpp-dev
